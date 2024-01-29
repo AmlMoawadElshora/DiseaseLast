aacb6d636761b641016fa03c07840c9925116083
AmlMoawadElshora <ineedtorun127@gmail.com>
2024-01-29 16:34:45 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,39 +1,21 @@
 import os
-import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
 from PIL import Image
-import cv2
 from keras.models import load_model
 from flask import Flask, request, render_template
-from werkzeug.utils import secure_filename
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import requests
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-model_path = os.path.join(os.getcwd(), r'disease.h5')
-
-if not os.path.exists(model_path):
-    model_url = "https://github.com/AmlMoawadElshora/DiseaseLast/raw/main/disease.h5?download="
-    response = requests.get(model_url)
-    with open(model_path, 'wb') as f:
-        f.write(response.content)
-
-# Load the model
-model = load_model(model_path)
-
+model = load_model(r'disease.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 labels = {0: 'Healthy', 1: 'Powdery', 2: 'Rust'}
 
-# Ensure the 'uploads' directory exists
-uploads_dir = os.path.join(os.getcwd(), 'uploads')
-if not os.path.exists(uploads_dir):
-    os.makedirs(uploads_dir)
-
-def getResult(image_path):
-    img = load_img(image_path, target_size=(225, 225))
+def getResult(img):
+    img = img.resize((225, 225))
     x = img_to_array(img)
     x = x.astype('float32') / 255.
     x = np.expand_dims(x, axis=0)
@@ -44,22 +26,20 @@ def getResult(image_path):
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        f = request.files['file']
+    if 'file' not in request.files:
+        return "No file part"
 
-        # Ensure the 'uploads' directory exists
-        if not os.path.exists(uploads_dir):
-            os.makedirs(uploads_dir)
+    file = request.files['file']
 
-        file_path = os.path.join(uploads_dir, secure_filename(f.filename))
-        f.save(file_path)
-        predictions = getResult(file_path)
-        predicted_label = labels[np.argmax(predictions)]
-        return str(predicted_label)
-    return None
+    if file.filename == '':
+        return "No selected file"
+
+    img = Image.open(file.stream)
+    predictions = getResult(img)
+    predicted_label = labels[np.argmax(predictions)]
+    return str(predicted_label)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
